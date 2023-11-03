5aa478da90ce437470b8c09355b3d080543720c3
pablo <pablomedranogoo@gmail.com>
2023-11-03 15:41:10 -0300

    iniciojs

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log("3. Soy el js de la pagina externa");
+console.log("=========================================");
+
+/*comentarios varias lines*/
+
+// comentario de una sola linea
+
+// 1. variables /palabras reservadas: son comandos reservados de js
+
+//declaraciones de variables
+var nombre;
+
+let email;
+
+const DNI = 12345668;
+
+//asignacion del valor de lña variable
+nombre = "Pepe";
+
+email = "pp@gmail.com";
+
+console.log(nombre);
+
+//dos cosas a la vez
+var edad = 20;
+
+nombre = "mario";
+
+console.log(nombre);
+
+console.log("============================================");
+
+// Arreglos - arraus
+// indice        0       1        2        3        4
+let alumnos = ["pepe", "mario", "juan", "pedro", "pablo"];
+
+console.log(alumnos);
+
+console.log("el alumno en la pocion 0 con el indice 0 es: " + [0]);
+
+console.log();
+console.log();
+console.log();
+
